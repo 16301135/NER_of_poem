be6754491cdbb3d9f86ba0d77adebea801129695
rebecca <16301135@bjtu.edu.cn>
2020-03-30 20:17:36 +0800

    增加模型搭建

---
+++ utils.py
@@ -0,0 +1,9 @@
+# 数据相关的配置
+event_type = "cluener"
+
+train_file_path = "./data/%s.train" % event_type
+dev_file_path = "./data/%s.dev" % event_type
+test_file_path = "./data/%s.dev" % event_type
+
+# 模型相关的配置
+MAX_SEQ_LEN = 128   # 输入的文本最大长度
